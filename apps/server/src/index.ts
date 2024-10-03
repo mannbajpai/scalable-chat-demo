b6a4ddd4f484ec7a6ee3a7bee565fa988b3be9f1
mannbajpai <mannbajpai.grs@gmail.com>
2024-10-03 19:57:25 +0530

    socket server init

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello From Server");
+import http from "http";
+import SocketService from "./services/socket";
+async function init() {
+    const socketService = new SocketService();
+  const httpServer = http.createServer();
+  const PORT = process.env.PORT || 8000;
+
+  socketService.io.attach(httpServer);
+
+  httpServer.listen(PORT, () =>
+    console.log(`HTTP server running on Port ${PORT}`)
+  );
+}
+
+init();
